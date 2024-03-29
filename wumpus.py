83a3a1329a710f675421666427c7c0deb80ef5bd
Michael Haynes <antihaynes@gmail.com>
2024-03-29 12:49:30 -0500

    wumpus: correct but TLE

--- wumpus.py
+++ wumpus.py
@@ -11,160 +11,144 @@ class YouDie(Exception):
 
 
 class WumpusCave:
-    WIDTH = 4
-    HEIGHT = 4
-
     class Room:
-        def __init__(self, cave, i, j):
+        def __init__(self, cave, coords, char):
             self.cave = cave
-            self.i, self.j = i, j
-            self.adj = set()
-            self.contents = set()
+            self.coords = coords
+            self.has_wumpus = char == "W"
+            self.has_gold = char == "G"
+            if char == "P":
+                self.cave.pits.add(self)
 
-        @property
-        def coords(self):
-            return self.i, self.j
+        def neighbors(self):
+            i, j = self.coords
+            yield from (
+                self.cave[ni, nj]
+                for d in (-1, 1)
+                for ni, nj in ((i + d, j), (i, j + d))
+                if (ni, nj) in self.cave
+            )
 
         def is_deathtrap(self):
-            return self is self.cave.wumpus_room or self in self.cave.pits
+            return self.has_wumpus or self in self.cave.pits
 
         def smells_like_wumpus(self):
-            return any(r is self.cave.wumpus_room for r in self.adj)
+            return any(r.has_wumpus for r in self.neighbors())
 
         def is_suspiciously_breezy(self):
-            return any(r in self.cave.pits for r in self.adj)
+            return any(r in self.cave.pits for r in self.neighbors())
 
         def is_boring(self):
-            return (
-                not self.smells_like_wumpus()
-                and not self.is_suspiciously_breezy()
-                and not self.is_deathtrap()
+            return not (
+                self.smells_like_wumpus()
+                or self.is_suspiciously_breezy()
+                or self.is_deathtrap()
             )
 
         def __hash__(self):
             return hash(self.coords)
 
-        def __eq__(self, other):
-            return self.coords == other.coords
-
         def __repr__(self):
             return f"<CaveRoom {self.coords}>"
 
     def __init__(self, cave):
+        self.width = len(cave[0])
+        self.pits = set()
         self._rooms = {
-            (i, j): self.Room(self, i, j)
-            for i in range(self.HEIGHT)
-            for j in range(self.WIDTH)
+            (i, j): self.Room(self, (i, j), char)
+            for i, row in enumerate(cave)
+            for j, char in enumerate(row)
         }
         self.start = self._rooms[0, 0]
-        self.pits = set()
-        for i, row in enumerate(cave):
-            for j, contents in enumerate(row):
-                room = self._rooms[i, j]
-                room.contents.add(contents)
-                for ni, nj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
-                    if (ni, nj) in self._rooms:
-                        room.adj.add(self._rooms[ni, nj])
-                if "W" in room.contents:
-                    self.wumpus_room = room
-                if "P" in room.contents:
-                    self.pits.add(room)
-                if "G" in room.contents:
-                    self.gold_room = room
+
+    def __iter__(self):
+        yield from self._rooms.values()
+
+    def __contains__(self, item):
+        if isinstance(item, self.Room):
+            return self._rooms[item.coords] is item
+        return item in self._rooms
+
+    def __len__(self):
+        return len(self._rooms)
 
     def __getitem__(self, coords):
         return self._rooms[coords]
 
-    @property
-    def rooms(self):
-        return self._rooms.values()
 
-    def kill_wumpus(self):
-        self.wumpus_room = None
-
-
-class CaveInfo:
-    def __init__(self, num_pits):
-        self.wumpus_states = [1 << i for i in range(1, 16)]
-        self.pit_states = [
-            n for n in range(2**16) if n.bit_count() == num_pits and n % 2 == 0
-        ]
-
-    @property
-    def maybe_pit(self):
-        return reduce(lambda r, n: r | n, self.pit_states, 0)
-
-    def update(self, room):
-        if room.is_deathtrap():
-            raise YouDie(f"Oh no, you died in {room}!")
-        mask = sum(1 << (4 * n.i + n.j) for n in room.adj)
-        self.wumpus_states = self._constrain(
-            self.wumpus_states, mask, room.smells_like_wumpus()
+class CaveMap:
+    def __init__(self, cave):
+        self._bitmasks = {
+            room: 1 << (cave.width * room.coords[0] + room.coords[1]) for room in cave
+        }
+        self._rooms = {mask: room for room, mask in self._bitmasks.items()}
+        self.accessible, self.visited = {cave.start}, set()
+        self.maybe_wumpus = sum(
+            m for r, m in self._bitmasks.items() if r is not cave.start
         )
-        self.pit_states = self._constrain(
-            self.pit_states, mask, room.is_suspiciously_breezy()
-        )
-        # self.maybe_pit = reduce(lambda r, n: r | n, self.pit_states, 0)
-        # apply "wumpus not in pit" constraint
-        self.wumpus_states = self._constrain(
-            self.wumpus_states, reduce(lambda r, n: r & n, self.pit_states, 1), False
-        )
+        self.pit_states = {
+            n
+            for n in range(2 ** len(cave))
+            if n.bit_count() == len(cave.pits) and not n & self._bitmasks[cave.start]
+        }
+        self._update_pitmask()
 
-    def kill_wumpus(self):
-        self.pit_states = self._constrain(
-            self.pit_states, self.wumpus_states.pop(), False
+    def _update_pitmask(self):
+        self.maybe_pit = reduce(lambda r, n: r | n, self.pit_states, 0)
+
+    def locate_wumpus(self):
+        return self._rooms[self.maybe_wumpus]
+
+    def visit(self, room):
+        neighbs = sum(self._bitmasks[n] for n in room.neighbors())
+        self.pit_states = {
+            m
+            for m in self.pit_states
+            if bool(m & neighbs) == room.is_suspiciously_breezy()
+        }
+        if self.maybe_wumpus:
+            self.maybe_wumpus &= neighbs if room.smells_like_wumpus() else ~neighbs
+            # the wumpus can't be in a pit
+            self.maybe_wumpus &= ~reduce(lambda r, n: r & n, self.pit_states, 1)
+            self.pit_states = {m for m in self.pit_states if ~m & self.maybe_wumpus}
+        self._update_pitmask()
+        self.accessible.update(room.neighbors())
+        self.visited.add(room)
+
+    def definitely_safe(self):
+        yield from (
+            r
+            for r, m in self._bitmasks.items()
+            if not (m & self.maybe_wumpus or m & self.maybe_pit)
         )
-        assert not self.wumpus_states, "You prematurely killed the wumpus, you jerk"
-        logger.info("You killed the wumpus!")
-
-    def is_definitely_safe(self, room):
-        mask = 1 << (4 * room.i + room.j)
-        return mask not in self.wumpus_states and not (mask & self.maybe_pit)
-
-    @classmethod
-    def _constrain(cls, masks, constraint, target_bool):
-        return {m for m in masks if bool(m & constraint) == target_bool}
 
 
 class Explorer:
     def __init__(self, cave: WumpusCave):
-        self.cave = cave
+        self.map = CaveMap(cave)
         self.pos = cave.start
-        self.explored = set()
-        self.accessible = {cave.start}
-        self.info = CaveInfo(len(cave.pits))
 
     def kill_wumpus(self):
-        self.info.kill_wumpus()
-        self.cave.kill_wumpus()
+        self.map.maybe_wumpus = 0
 
-    def take_stock(self):
-        self.info.update(self.pos)
-        self.explored.add(self.pos)
-        self.accessible.update(self.pos.adj)
-        if (
-            len(self.info.wumpus_states) == 1
-            and self.cave.wumpus_room in self.accessible
-        ):
-            self.kill_wumpus()
-
-    def move_to(self, room):
-        if room in self.accessible:
-            self.pos = room
-            self.info.update(self.pos)
-            self.take_stock()
-        else:
-            raise ValueError(f"Room {room} is not accessible")
-
-    def choices(self):
-        yield from filter(
-            self.info.is_definitely_safe, self.accessible.difference(self.explored)
+    def move(self):
+        self.pos = next(
+            iter(
+                self.map.accessible.difference(self.map.visited).intersection(
+                    self.map.definitely_safe()
+                )
+            )
         )
 
     def find_gold(self):
-        while self.pos is not self.cave.gold_room:
-            self.take_stock()
-            self.pos = next(self.choices())
+        while not self.pos.has_gold:
+            self.map.visit(self.pos)
+            try:
+                self.map.locate_wumpus()
+                self.kill_wumpus()
+            except KeyError:
+                pass
+            self.move()
 
 
 def wumpus_world(cave):
